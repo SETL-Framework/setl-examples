f6bbd90ec2a816fedfc0577c72aa5d3b7f251663
truongj <joris.truong@jcdecaux.com>
2020-11-20 15:39:37 +0100

    feat: added exercise object for transform

---
+++ src/main/scala/com/github/joristruong/transform/exercise/App.scala
@@ -0,0 +1,13 @@
+package com.github.joristruong.transform.exercise
+
+import com.jcdecaux.setl.Setl
+import com.jcdecaux.setl.config.ConfigLoader
+
+/**
+ * Learn about Transform processes with SETL
+ */
+object App {
+  def main(args: Array[String]): Unit = {
+    
+  }
+}
